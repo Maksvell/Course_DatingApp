1df4b6fc66bc2089d016891935e46f6ad1990d2a
Maksvell <ivax204@gmail.com>
2024-07-30 21:14:49 +0300

    End of section 11 (adding photo upload functionality)

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,27 @@
-﻿namespace API;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
-public class IdentityServiceExtension
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
 {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
+        return services;
+    }
 }
