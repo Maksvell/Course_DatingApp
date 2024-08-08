18833ed60f94241e2166751847c18ce020d4b4a1
Maksvell <ivax204@gmail.com>
2024-08-08 16:20:22 +0300

    End of section 18 (unit of work and finishing touches)

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -7,7 +7,6 @@ namespace API.Interfaces;
 public interface IUserRepository
 {
     void Update(AppUser user);
-    Task<bool> SaveAllAsync();
     Task<IEnumerable<AppUser>> GerUsersAsync();
     Task<AppUser?> GetUserByIdAsync(int id);
     Task<AppUser?> GetUserByUsernameAsync(string username);
